a82421714f83012e7c53c2af07d23a574b78df18
David Muriithi <muriithid05@gmail.com>
2024-07-30 01:37:08 +0300

    minor fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,35 +1,40 @@
 #!/usr/bin/python3
-""" log parsing, IO in python """
 import sys
+import re
 
 
 def print_data(total_file_size, status_code_data):
-    """prints total size and status code count"""
-    print('File size: {}'.format(total_file_size))
+    """Prints total size and status code count"""
+    print("File size: {}".format(total_file_size))
     for k, v in sorted(status_code_data.items()):
         if v != 0:
-            print('{}: {}'.format(k, v))
+            print("{}: {}".format(k, v))
 
 
-status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
 status_code_data = {code: 0 for code in status_codes}
 total_file_size = 0
+count = 0
+
+log_pattern = re.compile(
+    r'\S+ - \[\S+ \S+\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)'
+)
+
 try:
-    count = 0
     for line in sys.stdin:
-        splitstr = line.split()
-        try:
-            total_file_size += int(splitstr[-1])
-            code = splitstr[-2]
+        match = log_pattern.match(line)
+        if match:
+            code, size = match.groups()
+            total_file_size += int(size)
             if code in status_code_data:
-                count += 1
                 status_code_data[code] += 1
-                if count % 10 == 0:
-                    print_data(total_file_size, status_code_data)
-        except:
-            pass
+            count += 1
+            if count % 10 == 0:
+                print_data(total_file_size, status_code_data)
 except KeyboardInterrupt:
     print_data(total_file_size, status_code_data)
     raise
-else:
+except Exception as e:
+    pass
+finally:
     print_data(total_file_size, status_code_data)
