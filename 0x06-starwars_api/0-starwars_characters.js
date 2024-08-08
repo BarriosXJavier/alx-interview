c4f03480589f20e12f5aac0107f0a9d74d8338c0
David Muriithi <muriithid05@gmail.com>
2024-08-08 19:16:10 +0300

    new file:   .gitignore 	modified:   0-starwars_characters.js 	new file:   package.json

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,43 @@
 #!/usr/bin/node
-const argv = process.argv;
-const urlFilm = 'https://swapi-api.alx-tools.com/api/';
-const urlMovie = `${urlFilm}${argv[2]}/`;
 
+const argv = process.argv;
 const request = require('request');
 
+// Check if a movie ID is provided
+if (argv.length < 3) {
+    console.error('Usage: ./<script> <movie_id>');
+    process.exit(1);
+}
+
+const movieId = argv[2];
+const urlFilm = 'https://swapi-api.alx-tools.com/api/films/';
+const urlMovie = `${urlFilm}${movieId}/`;
+
 request(urlMovie, function (error, response, body) {
-    if (error == null) {
+    if (!error) {
         const fbody = JSON.parse(body);
         const characters = fbody.characters;
 
         if (characters && characters.length > 0) {
             const limit = characters.length;
-            CharRequest(0, characters[0], characters, limit);
+            charRequest(0, characters, limit);
         }
     } else {
-        console.log(error);
+        console.error('Error:', error);
     }
 });
 
-function CharRequest(idx, url, characters, limit) {
+function charRequest(idx, characters, limit) {
     if (idx === limit) {
         return;
     }
-    request(url, function (error, response, body) {
+    request(characters[idx], function (error, response, body) {
         if (!error) {
             const rbody = JSON.parse(body);
             console.log(rbody.name);
-            idx++;
-            CharRequest(idx, characters[idx], characters, limit);
+            charRequest(idx + 1, characters, limit);
         } else {
-            console.error('error:', error);
+            console.error('Error:', error);
         }
     });
 }
