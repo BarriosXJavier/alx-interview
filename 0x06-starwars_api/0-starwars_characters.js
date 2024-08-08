0b49789ab30608504f98c2739b9713b70be46917
David Muriithi <muriithid05@gmail.com>
2024-08-08 18:49:42 +0300

    Refactor: fix error handling

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,48 @@
 #!/usr/bin/node
-const argv = process.argv;
-const url_film = 'https://swapi-api.hbtn.io/api/films/';
-const url_movie = `${urlFilm}${argv[2]}/`;
 
-import request from 'request';
+'use strict'
 
-request(url_movie, function (error, response, body) {
-    if (error == null) {
-        const fbody = JSON.parse(body);
-        const characters = fbody.characters;
+import { get } from 'https'
 
-        if (characters && characters.length > 0) {
-            const limit = characters.length;
-            CharRequest(0, characters[0], characters, limit);
-        }
-    } else {
-        console.log(error);
-    }
-});
-
-function CharRequest(idx, url, characters, limit) {
-    if (idx === limit) {
-        return;
-    }
-    request(url, function (error, response, body) {
-        if (!error) {
-            const rbody = JSON.parse(body);
-            console.log(rbody.name);
-            idx++;
-            CharRequest(idx, characters[idx], characters, limit);
-        } else {
-            console.error('error:', error);
-        }
-    });
+if (process.argv.length < 3) {
+    console.error('Usage: node script.js <movie_id>')
+    process.exit(1)
 }
+
+const movieId = parseInt(process.argv[2], 10)
+
+const options = {
+    hostname: 'swapi-api.alx-tools.com',
+    path: `/api/films/${movieId}/`,
+    method: 'GET'
+}
+
+get(options, (res) => {
+    let data = ''
+
+    res.on('data', (chunk) => {
+        data += chunk
+    })
+
+    res.on('end', () => {
+        const { characters } = JSON.parse(data)
+        characters.forEach((characterUrl) => {
+            get(characterUrl, (charRes) => {
+                let charData = ''
+
+                charRes.on('data', (chunk) => {
+                    charData += chunk
+                })
+
+                charRes.on('end', () => {
+                    const { name } = JSON.parse(charData)
+                    console.log(name)
+                })
+            }).on('error', (err) => {
+                console.error(`Error fetching character: ${err.message}`)
+            })
+        })
+    })
+}).on('error', (err) => {
+    console.error(`Error fetching movie data: ${err.message}`)
+})
