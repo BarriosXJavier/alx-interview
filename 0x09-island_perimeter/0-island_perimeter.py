eec278c6ce676542157f85e726eabf33ecdcae5f
David Muriithi <muriithid05@gmail.com>
2024-08-27 10:38:34 +0300

    refactor: pycodestyle

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,22 +7,27 @@ in a grid of water and land cells.
 
 def island_perimeter(grid):
     """
-    Calculate the perimeter of an island in a grid of water and land cells with no lakes.
+    Calculate the perimeter of an island in a grid of
+    water and land cells with no lakes.
     """
     perimeter = 0
-    if type(grid) != list:
+    if not isinstance(grid, list):
         return 0
     num_rows = len(grid)
-    for row_index, row in enumerate(grid):
+    for row_idx, row in enumerate(grid):
         num_cols = len(row)
-        for col_index, cell in enumerate(row):
+        for col_idx, cell in enumerate(row):
             if cell == 0:
                 continue
             edges = (
-                row_index == 0 or (len(grid[row_index - 1]) > col_index and grid[row_index - 1][col_index] == 0),
-                col_index == num_cols - 1 or (num_cols > col_index + 1 and row[col_index + 1] == 0),
-                row_index == num_rows - 1 or (len(grid[row_index + 1]) > col_index and grid[row_index + 1][col_index] == 0),
-                col_index == 0 or row[col_index - 1] == 0,
+                row_idx == 0 or (len(grid[row_idx - 1]) > col_idx and
+                                 grid[row_idx - 1][col_idx] == 0),
+                col_idx == num_cols - 1 or (num_cols > col_idx + 1 and
+                                            row[col_idx + 1] == 0),
+                row_idx == num_rows - 1 or (len(grid[row_idx + 1]) >
+                                            col_idx and
+                                            grid[row_idx + 1][col_idx] == 0),
+                col_idx == 0 or row[col_idx - 1] == 0,
             )
             perimeter += sum(edges)
     return perimeter
